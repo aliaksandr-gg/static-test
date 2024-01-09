53eddb7421ecfd3477859cb44b5ecb6fad7f7c3b
Aliaksandr <aliaksandr.gg@gmail.com>
2024-01-09 22:04:40 +0100

    t1

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,6 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -51,7 +50,6 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-
     /**
      * The attributes that should be hidden for serialization.
      *
